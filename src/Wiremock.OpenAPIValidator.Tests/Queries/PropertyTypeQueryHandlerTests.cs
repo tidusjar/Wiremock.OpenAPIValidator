392b8f1ad92e3a0540f59af6c5ba4f30eeffa1ea
Jamie Rees <Jamie.Rees@exclaimer.com>
2022-06-29 13:48:24 +0100

    test: more coverage

--- src/Wiremock.OpenAPIValidator.Tests/Queries/PropertyTypeQueryHandlerTests.cs
+++ src/Wiremock.OpenAPIValidator.Tests/Queries/PropertyTypeQueryHandlerTests.cs
@@ -90,7 +90,66 @@ public class PropertyTypeQueryHandlerTests
         });
     }
 
-    private static OpenApiResponses RequiredOpenApiSchema()
+    [Test]
+    public async Task Handle_MissingPropPassedIn()
+    {
+        var wiremockProps = new WiremockResponseProperties();
+        wiremockProps.Properties.Add("Prop2", typeof(int));
+
+        var response = await _handler.Handle(new PropertyTypeQuery
+        {
+            Responses = null,
+            MockProperties = wiremockProps,
+            Name = "UnitTest"
+        }, CancellationToken.None);
+
+        Assert.That(response, Has.Count.EqualTo(0));
+    }
+
+    [Test]
+    public async Task Handle_MissingPropPassedInMock()
+    {
+        OpenApiResponses apiResponse = RequiredOpenApiSchema();
+
+        var response = await _handler.Handle(new PropertyTypeQuery
+        {
+            Responses = apiResponse,
+            MockProperties = null,
+            Name = "UnitTest"
+        }, CancellationToken.None);
+
+        Assert.That(response, Has.Count.EqualTo(0));
+    }
+
+    [TestCase("date-time")]
+    [TestCase("uuid")]
+    [TestCase("number")]
+    [TestCase("integer")]
+    public async Task Handle_MissingPropType(string type)
+    {
+        var wiremockProps = new WiremockResponseProperties();
+        wiremockProps.Properties.Add("Prop1", typeof(Assert));
+
+        OpenApiResponses apiResponse = RequiredOpenApiSchema(type);
+
+        var response = await _handler.Handle(new PropertyTypeQuery
+        {
+            Responses = apiResponse,
+            MockProperties = wiremockProps,
+            Name = "UnitTest"
+        }, CancellationToken.None);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(response, Has.Count.EqualTo(1));
+            Assert.That(response[0].Type, Is.EqualTo(ValidatorType.ResponsePropertyType));
+            Assert.That(response[0].Name, Is.EqualTo("Response - UnitTest - prop1"));
+            Assert.That(response[0].ValidationResult, Is.EqualTo(ValidationResult.Failed));
+            Assert.That(response[0].Description, Is.Not.Null.And.Contains("prop1"));
+        });
+    }
+
+    private static OpenApiResponses RequiredOpenApiSchema(string type = "string")
     {
         return new OpenApiResponses
         {
@@ -106,7 +165,7 @@ public class PropertyTypeQueryHandlerTests
                             Properties = new Dictionary<string, OpenApiSchema>
                             {
                                 {"prop1", new OpenApiSchema {
-                                    Type = "string"
+                                    Type = type
                                 }}
                             }
                         }
