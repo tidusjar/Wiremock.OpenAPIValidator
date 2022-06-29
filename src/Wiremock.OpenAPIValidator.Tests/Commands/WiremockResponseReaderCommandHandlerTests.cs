2d614c921e76357ad76daebddd4f36858203dbac
Jamie Rees <Jamie.Rees@exclaimer.com>
2022-06-29 12:47:47 +0100

    test: test

---
+++ src/Wiremock.OpenAPIValidator.Tests/Commands/WiremockResponseReaderCommandHandlerTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wiremock.OpenAPIValidator.Commands;
+
+namespace Wiremock.OpenAPIValidator.Tests.Commands
+{
+    public class WiremockResponseReaderCommandHandlerTests
+    {
+        private WiremockResponseReaderCommandHandler _handler;
+
+        [SetUp]
+        public void Setup()
+        {
+            _handler = new WiremockResponseReaderCommandHandler();
+        }
+
+        [Test]
+        public async Task Handle_BadDirectory()
+        {
+            var response = await _handler.Handle(new WiremockResponseReaderCommand
+            {
+                WiremockMappingPath = string.Empty
+            }, CancellationToken.None);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(response, Is.Not.Null);
+            });
+        }
+    }
+}
